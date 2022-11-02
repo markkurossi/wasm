5acbd813a76649af4294f5fab18b4f2ffe31304b
Markku Rossi <mtr@iki.fi>
2022-11-02 07:17:13 +0100

    Module documentation.

--- module/module.go
+++ module/module.go
@@ -6,6 +6,10 @@
 // All rights reserved.
 //
 
+// Package module implements WebAssembly modules and provides data
+// structures to represent the decoded module. It also provides
+// functions for encoding and decoding modules in binary and text
+// formats.
 package module
 
 import (
