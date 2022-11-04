eac8e556573f85c45c44503fb1e06559deea51e0
Markku Rossi <mtr@iki.fi>
2022-11-04 14:48:35 +0100

    Refactored source files.

--- binary/binary.go
+++ binary/binary.go
@@ -1,15 +1,11 @@
 //
-// module.go
+// binary.go
 //
 // Copyright (c) 2022 Markku Rossi
 //
 // All rights reserved.
 //
 
-// Package module implements WebAssembly modules and provides data
-// structures to represent the decoded module. It also provides
-// functions for encoding and decoding modules in binary and text
-// formats.
 package module
 
 import (
---
+++ binary/module.go
@@ -0,0 +1,16 @@
+//
+// module.go
+//
+// Copyright (c) 2022 Markku Rossi
+//
+// All rights reserved.
+//
+
+// Package module implements WebAssembly modules and provides data
+// structures to represent the decoded module. It also provides
+// functions for encoding and decoding modules in binary and text
+// formats.
+package module
+
+type Module struct {
+}
